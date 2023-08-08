f16b113b0a385cd1922c06fb7271619edf745abd
mikeoo1 <micthomas407@gmail.com>
2023-08-08 06:18:51 +0100

    update

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,11 +1,13 @@
 const express = require("express")
-const{newTask,assignTask,acceptTask,getAllTasks,getOneTask}=require("../controllers/taskController")
+const{newTask,assignTask,acceptTask,getAllTasks,getOneTask,updateTask,deleteTask}=require("../controllers/taskController")
 const Router =express()
 
 Router.route("/newTask/:id").put(newTask)
 Router.route("/accept/:id").post(assignTask)
 Router.route("/getall").get(getAllTasks)
 Router.route("/getone/:id").get(getOneTask)
+Router.route("/update/:id").put(updateTask)
+Router.route("/delete/:id").delete(deleteTask)
 
 // Router.route("/accepts").post(acceptTask)
 
