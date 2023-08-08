5e4d73dde5f8f3952bdb459ff4ce9044a83bfbc8
mikeoo1 <micthomas407@gmail.com>
2023-08-08 06:24:08 +0100

    update

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -4,7 +4,7 @@ const Router =express()
 
 Router.route("/newTask/:id").put(newTask)
 Router.route("/accept/:id").post(assignTask)
-Router.route("/getall").get(getAllTasks)
+Router.route("/getalltasks").get(getAllTasks)
 Router.route("/getone/:id").get(getOneTask)
 Router.route("/update/:id").put(updateTask)
 Router.route("/delete/:id").delete(deleteTask)
